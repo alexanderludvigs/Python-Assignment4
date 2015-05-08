6f195e7c0b59803f8bde0870cdeae714861eb35f
alexanderludvgis <alexander10@ru.is>
2015-05-08 01:29:15 +0000

    main is now executable

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
+#!C:\Python34\python
+
 __author__ = 'Alexander'
 
 import os
 import re
-import sys
 
 
 def main():
 
+    #for user.
+    #path, show = map(str, input("Enter path and TV show. format = path,show: ").split(','))
+    #print('Entered path: ', path)
+    #print('Entered show ', show)
+
     aviFiles = re.compile(r'.*\.avi$')
+    #for fixing input:
+    splitter = re.compile(r'[.-]+')
+
+    #for programmer.
+    path = 'downloads'
+    show = 'tmp'
+
+    show = ' '.join(splitter.split(show))
 
 
     l=[]
-    for root, dirs, files in os.walk('downloads'):
+    for root, dirs, files in os.walk(path):
         for name in files:
             if aviFiles.search(name):
                 l.append(os.path.join(root, name))
 
     #prints all file.avi paths in downloads(root)
-    for i in l:
-        print(i)
+    #for i in l:
+        #print(i)
 
 
-    path = os.path.join('downloads', 'tmp')
-    print(path)
-
-    #creates downloads\tmp folder if it does not exist
-    if not os.path.exists(path):
-        os.mkdir(path)
+    #creates folder if it does not exist
+    folder = os.path.join(path, show)
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        print('%s was created' % folder)
 
 
 
