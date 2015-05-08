cc03f01b7938f5dd6bc0bdb01a409514303e620c
alexanderludvgis <alexander10@ru.is>
2015-05-08 19:48:46 +0000

    sort by title and sort by season klárt

--- main.py
+++ main.py
@@ -4,42 +4,73 @@ __author__ = 'Alexander'
 
 import os
 import re
-
+import shutil
 
 def main():
 
-    #for user.
+    #input from user:
     #path, show = map(str, input("Enter path and TV show. format = path,show: ").split(','))
     #print('Entered path: ', path)
     #print('Entered show ', show)
 
-    aviFiles = re.compile(r'.*\.avi$')
-    #for fixing input:
-    splitter = re.compile(r'[.-]+')
+    splitter = re.compile(r'[.-]+')     #input splitter
 
-    #for programmer.
+    #input:
     path = 'downloads'
-    show = 'tmp'
+    show = 'house'
 
     show = ' '.join(splitter.split(show))
 
+    #regular expressions:
+    ignore   = re.compile(r'[^nfo$|sfv$]')  #files we want to ignore
+    #aviFiles = re.compile(r'.*\.avi$')  #.avi files
+    season   = re.compile(r's(\d{1,2})', re.I)          #format: House.S08E03.HDTV.XviD-LOL
+    season2  = re.compile(r'([1-9])\d\d')               #format:  house.805.hdtv-lol
+    title    = re.compile(r'(^%s)[ -.]' % show, re.I)   #Title of show
 
-    l=[]
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if aviFiles.search(name):
-                l.append(os.path.join(root, name))
-
-    #prints all file.avi paths in downloads(root)
-    #for i in l:
-        #print(i)
-
-
-    #creates folder if it does not exist
+    #creates folder with incoming name if it does not exist:
     folder = os.path.join(path, show)
     if not os.path.exists(folder):
         os.mkdir(folder)
         print('%s was created' % folder)
+    else:
+        print('%s exists!' % folder)
+
+    #sort by title:
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            splittedName = ' '.join(splitter.split(name))
+
+            if title.search(splittedName):
+                src = os.path.join(root, name)
+                des = os.path.join(folder, name)
+                shutil.move(src, des)
+            else:
+                continue
+
+    #sort by season:
+    for root, dirs, files in os.walk(folder):
+        for name in files:
+
+            if season.search(name):
+                snum = int(season.search(name).group(1))
+            elif season2.search(name):
+                snum = int(season2.search(name).group(1))
+            else:
+                continue
+
+            seasondir = 'Season ' + str(snum)
+            seasondir = os.path.join(folder, seasondir)
+
+            src = os.path.join(root, name)
+            des = os.path.join(seasondir, name)
+
+            if not os.path.exists(seasondir):
+                os.mkdir(seasondir)
+                shutil.move(src, des)
+
+            else:
+                shutil.move(src, des)
 
 
 
